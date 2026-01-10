f091a4539eb1cb6b460fc5e9676198daa942715b
EdsonJuni0r <vianaedsonjunior@gmail.com>
2026-01-10 12:08:47 -0400

    Updates

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 'use strict';
 require('dotenv').config();
 const express = require('express');
-const pug = require('pug');
+const path = require('path');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
@@ -10,7 +10,7 @@ const app = express();
 
 
 app.set('view engine', 'pug');
-app.set('views', './views/pug');
+app.set('views', path.join(__dirname, './views/pug'));
 fccTesting(app); // For fCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
